2c693df08b1646a4b24483b4aa867fb734ab3647
marcos7-eri <cchoqueerick249@gmail.com>
2025-11-13 21:33:28 -0400

    H

--- examenweb2parcial/examenweb2parcial/Controllers/TareasController.cs
+++ examenweb2parcial/examenweb2parcial/Controllers/TareasController.cs
@@ -15,15 +15,11 @@ namespace examenweb2parcial.Controllers
         {
             _context = context;
         }
-
-        // 1 - Listar tareas
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Tarea>>> GetTareas()
         {
             return await _context.Tareas.Include(t => t.Miembro).Include(t => t.Prioridad).ToListAsync();
         }
-
-        // 2 - Crear tarea
         [HttpPost]
         public async Task<ActionResult<Tarea>> CrearTarea(Tarea tarea)
         {
@@ -33,7 +29,6 @@ namespace examenweb2parcial.Controllers
             return CreatedAtAction(nameof(GetTareaPorId), new { id = tarea.Id }, tarea);
         }
 
-        // 3 - Editar tarea
         [HttpPut("{id}")]
         public async Task<ActionResult> EditarTarea(int id, Tarea tarea)
         {
@@ -53,8 +48,6 @@ namespace examenweb2parcial.Controllers
             await _context.SaveChangesAsync();
             return NoContent();
         }
-
-        // GET por Id
         [HttpGet("{id}")]
         public async Task<ActionResult<Tarea>> GetTareaPorId(int id)
         {
@@ -62,22 +55,16 @@ namespace examenweb2parcial.Controllers
             if (tarea == null) return NotFound();
             return tarea;
         }
-
-        // 5 Ordenar por prioridad
         [HttpGet("ordenar-por-prioridad")]
         public async Task<ActionResult<IEnumerable<Tarea>>> GetTareasPorPrioridad()
         {
             return await _context.Tareas.OrderBy(t => t.Prioridad.Nombre).ToListAsync();
         }
-
-        // 6 - Buscar por titulo
         [HttpGet("buscar")]
         public async Task<ActionResult<IEnumerable<Tarea>>> BuscarPorTitulo([FromQuery] string titulo)
         {
             return await _context.Tareas.Where(t => t.Titulo.Contains(titulo)).ToListAsync();
         }
-
-        // 7 - Listar tareas asignadas por miembro
         [HttpGet("por-miembro/{miembroId}")]
         public async Task<ActionResult<IEnumerable<Tarea>>> ListarPorMiembro(int miembroId)
         {
